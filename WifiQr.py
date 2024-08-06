168351241e2b808d8420eb6b5e7d0ca5c0ddf6a4
Zainali005 <zainali5002@gmail.com>
2024-08-05 22:52:56 -0400

    yellow heart

--- WifiQr.py
+++ WifiQr.py
@@ -1,14 +1,11 @@
 from wifi_qrcode_generator import wifi_qrcode
 
-# Parameters for Wi-Fi QR code
-ssid = "Zain"  # Replace with your Wi-Fi SSID
-hidden = False     # Set to True if the network is hidden
-authentication_type = "WPA"  # Use "WPA" for WPA/WPA2-Personal
-password = "ALLAHisone!786"  # Replace with your Wi-Fi password
+ssid = "WIFI SSD"  # Replace with your Wi-Fi SSID
+hidden = False     
+authentication_type = "WPA"
+password = "your wifi password"  
 
-# Generate Wi-Fi QR code
 qr_code = wifi_qrcode(ssid, hidden=hidden, authentication_type=authentication_type, password=password)
 
-# Create and save QR code image
 qr_code_img = qr_code.make_image()
 qr_code_img.save("Wifi_qr_code.png")
